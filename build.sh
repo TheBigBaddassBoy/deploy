e3d53d248ab0cf6c76441813321fcba7d9b2f652
TheBigBaddassBoy <sahiddevcoronado@outlook.com>
2025-03-04 01:43:01 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+echo "from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='coron').exists():
+    User.objects.create_superuser('coron', 'coron@example.com', 'sahid15')
+    print('Superusuario coron creado con éxito.')
+else:
+    print('El superusuario coron ya existe.')" | python manage.py shell
